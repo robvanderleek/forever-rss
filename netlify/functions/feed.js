858253123639439216aca894648aed815716ec7b
Rob van der Leek <robvanderleek@gmail.com>
2022-04-01 06:26:25 +0200

    Hello XML parsing

--- netlify/functions/feed.js
+++ netlify/functions/feed.js
@@ -1,11 +1,14 @@
 const fetch = require("node-fetch");
+const {XMLParser} = require("fast-xml-parser");
 
 exports.handler = async function(event, context) {
     console.log('HERE');
     const result = await fetch('http://blog.cleancoder.com/atom.xml');
     let message = "Hello World";
     if (result.ok) {
-        message = await result.text();
+        const text = await result.text();
+        const xmlParser = new XMLParser();
+        message = xmlParser.parse(text);
     }
     return {
         statusCode: 200,

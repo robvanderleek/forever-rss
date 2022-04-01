9fafe587dfcdebe4e2ede5dd21d0032442a3b4d9
Rob van der Leek <robvanderleek@gmail.com>
2022-04-01 06:16:38 +0200

    Let's go with Netlify for now

--- netlify/functions/feed.js
+++ netlify/functions/feed.js
@@ -1,7 +1,14 @@
+const fetch = require("node-fetch");
+
 exports.handler = async function(event, context) {
     console.log('HERE');
+    const result = await fetch('http://blog.cleancoder.com/atom.xml');
+    let message = "Hello World";
+    if (result.ok) {
+        message = await result.text();
+    }
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
+        body: JSON.stringify({ message: message }),
     };
 }

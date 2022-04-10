86dbacb6287beef688ea619c8c39217f5ea8ebb5
Rob van der Leek <robvanderleek@gmail.com>
2022-04-10 21:45:37 +0200

    Load OPML from GitHub

--- netlify/functions/feeds.js
+++ netlify/functions/feeds.js
@@ -1,18 +1,19 @@
-const fs = require("fs");
 const {XMLParser} = require("fast-xml-parser");
-const path = require("path");
+const fetch = require("node-fetch");
 
 
-exports.handler = function (event, context) {
-    const opmlPath = path.join(__dirname, 'my.opml');
-    const opml = fs.readFileSync(opmlPath, 'utf8');
-    const options = {ignoreAttributes: false};
-    const xmlParser = new XMLParser(options);
-    const obj = xmlParser.parse(opml);
-    const outlines = obj.opml.body.outline;
-    const feeds = outlines.filter(o => '@_xmlUrl' in o).map(o => ({title: o['@_title'], url: o['@_xmlUrl']}));
-    return {
-        statusCode: 200,
-        body: JSON.stringify(feeds),
-    };
+exports.handler = async function (event, context) {
+    const response = await fetch('https://raw.githubusercontent.com/robvanderleek/robvanderleek/main/my-awesome.opml');
+    if (response.ok) {
+        const opml = await response.text();
+        const options = {ignoreAttributes: false};
+        const xmlParser = new XMLParser(options);
+        const obj = xmlParser.parse(opml);
+        const outlines = obj.opml.body.outline;
+        const feeds = outlines.filter(o => '@_xmlUrl' in o).map(o => ({title: o['@_title'], url: o['@_xmlUrl']}));
+        return {
+            statusCode: 200,
+            body: JSON.stringify(feeds),
+        };
+    }
 }

f15c9c71eb19fd53a421de83515a56b3cc6ad27a
Rob van der Leek <robvanderleek@gmail.com>
2022-04-21 06:14:34 +0200

    Fix test

--- src/components/EntriesList.test.js
+++ src/components/EntriesList.test.js
@@ -1,5 +1,5 @@
 import {formatDate} from "./EntriesList";
 
 test('format date', () => {
-    expect(formatDate('Sun, 19 Apr 2020 23:56:03 GMT')).toBe('Apr 20th 2020');
+    expect(formatDate('Sun, 19 Apr 2020 12:00:00 GMT')).toBe('Apr 19th 2020');
 });

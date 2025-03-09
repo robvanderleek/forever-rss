e6c9d5240c53ab8cf3129c9eeb9d432db8d17106
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-03-09 15:06:50 +0100

    fix: 🐛 Add redirect

--- forever_rss/app.py
+++ forever_rss/app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 from forever_rss.routes import user, oauth
@@ -11,6 +12,13 @@ app_api = FastAPI(title='Forever RSS API')
 app_api.add_middleware(RequestMiddleware)
 
 app.mount('/api/v1', app_api, name='api')
+
+
+@app.get("/oauth/redirect")
+def read_index():
+    return FileResponse(Path(__file__).resolve().parent.joinpath('web', 'dist', 'index.html'))
+
+
 app.mount('/', StaticFiles(directory=Path(__file__).resolve().parent.joinpath('web', 'dist'), html=True),
           name='static')
 
